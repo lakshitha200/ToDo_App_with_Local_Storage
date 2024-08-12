556ecfbd0a9137b0e95a33ad9e121ca7867e4297
lakshitha200 <lakshithaf20@gmail.com>
2024-08-12 20:34:56 +0530

    Expire date feature and some styles added. Listcontainer's styles chnaged.

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+// dates
+var today = new Date().toISOString().split('T')[0];
+document.getElementById("exprieDate").setAttribute('min',today);
+var expire = document.getElementById("exprieDate");
+
+
 //===== addTask() function you need to provide add Task btn using click event=====
 function addTask(){
-
     //check is there a value in the inputbox
     // ***if value is empty give alert***
     if(inputBox.value == ""){
-        alert("You should write something.")
-    }else{
+        alert("You should enter task.")
+    }else if(expire.value == ""){
+        alert("You should enter expire date.")
+    }
+    else{
     //***if there a value in inputbox do these things
 
         // 1- create li tag when add new task clicking the btn
         let li = document.createElement('li');
-        // 2 -add task value (eg: Php Fundamental) into this li tag
-        li.innerHTML = inputBox.value;
-        // 3- then append this li tag with value to our container
         listContainer.appendChild(li);
+
+        // 2 - inside the li, I am making two div tags.
+        let textDiv = document.createElement('div'); //to store task and close image buton
+        let dateDiv = document.createElement('div'); //to store start and end dates
+        textDiv.classList.add("tasks"); // i added a class to first div
+        dateDiv.classList.add("dates"); // i added a class to second div
+        li.appendChild(textDiv);
+        li.appendChild(dateDiv);
+
+        // 3 -add task value into this first div tag (textDiv)
+        let span = document.createElement('span'); //create span 
+        span.innerHTML = inputBox.value; //add task value to span
+        textDiv.appendChild(span); //add to first div
         
-        //as well ad need to add image (close img) to close task
+        //as well as we need to add image (close img) to close task inside the first div
         let img = document.createElement('img');
         img.src = "img/notcheck.png";
         img.classList.add("close"); //some css style add using class
-        li.appendChild(img); 
+        textDiv.appendChild(img); 
+
+        // 4 - add start date and expire date to second div (dateDiv)
+        let startDate = document.createElement('span');
+        startDate.innerText = "Start: "+ today;
+        let expireDate = document.createElement('span'); 
+        expireDate.innerHTML = `Expire: ${expire.value} <i class="fa-solid fa-pen-to-square" onclick="expireDateChange(this)"></i>`;
+        dateDiv.appendChild(startDate); 
+        dateDiv.appendChild(expireDate); 
+
     }
 
     //::::after adding task inputbox should be empty:::
     inputBox.value = "";
+    expire.value = "";
 
     //::::need to call saveData method to store these task in Local Storage
     saveData();
@@ -36,9 +64,10 @@ function addTask(){
 //===== when click listcontainer =======
 
 listContainer.addEventListener('click',(e)=>{
+  
     //when you click the task in the container, it mark as checked.
     // also you can uncheck
-    if(e.target.tagName === 'LI'){
+    if(e.target.tagName == 'LI' || e.target.tagName == 'DIV' ){
         e.target.classList.toggle("checked"); 
         //that changes shoud be saved in the local storage
         saveData();
@@ -46,10 +75,11 @@ listContainer.addEventListener('click',(e)=>{
 
     //when you click the close btn, task should be removed from the container
     else if(e.target.tagName === 'IMG'){
-        e.target.parentElement.remove();
+        e.target.parentElement.parentElement.remove();
         //that changes shoud be saved in the local storage
         saveData();
     }
+    
 })
 //------------------------------------------------------------------------
 //=====this function is used to save data in local storage.=====
@@ -68,3 +98,22 @@ function showList(){
 
 //you need to call to it
 showList();
+
+const dateChangeBox = document.getElementById("dateChange-box");
+const dark = document.getElementById("dark");
+const expireDateBox = document.getElementById("expireDate-box");
+function expireDateChange(element){
+    dateChangeBox.style.display = "block";
+    dark.style.display = "block";
+    if (element && element.parentElement) {
+        const expireDateText = element.parentElement.textContent.replace('Expire: ', '').trim();
+        expireDateBox.value = expireDateText;
+    } else {
+        console.error("Element or parentElement is not defined");
+    }
+};
+
+function closeWindow(){
+    dateChangeBox.style.display = "none";
+    dark.style.display = "none";
+};
